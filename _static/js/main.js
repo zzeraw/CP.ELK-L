2a28c63cec4e9a55363f30c19995cc7ffc4dfb44
zzeraw <pekidoch@gmail.com>
2015-04-28 21:07:59 +0300

    Предпродакшн

--- _static/js/main.js
+++ _static/js/main.js
@@ -1,3 +1,24 @@
-$(function(){
+$( function() {
     // $.stellar();
+    $( '.fancybox-image-link' ).fancybox();
+
+    var arr = [];
+
+    $( document ).scroll( function () {
+        var y = $( this ).scrollTop();
+
+        if (arr.length <= 1) {
+            arr.push(y);
+        }
+
+        if (arr.length > 1) {
+            if ( y > 600 ) {
+                $( '#navbarContacts' ).fadeIn();
+            } else {
+                $( '#navbarContacts' ).fadeOut();
+            }
+        }
+    });
+
 });
+
